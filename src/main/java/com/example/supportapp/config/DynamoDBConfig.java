6912f9d0ce874376cc4042c8fb4f544e11d9b7dc
yasmin-elderiny <118885295+yasmin-elderiny@users.noreply.github.com>
2024-12-05 11:26:00 +0200

    Fix issues and add healthcheck endpoint

--- src/main/java/com/example/supportapp/config/DynamoDBConfig.java
+++ src/main/java/com/example/supportapp/config/DynamoDBConfig.java
@@ -1,22 +1,27 @@
 package com.example.supportapp.config;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 @EnableDynamoDBRepositories(basePackages = "com.example.supportapp.repository")
 public class DynamoDBConfig {
+    public static final String SERVICE_ENDPOINT = "https://dynamodb.eu-west-1.amazonaws.com";
+    public static final String REGION = "eu-west-1";
 
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
         return AmazonDynamoDBClientBuilder.standard()
-                .withRegion("eu-west-1")
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("accessKey", "secretKey")))
+                .withEndpointConfiguration(
+                        new AwsClientBuilder.EndpointConfiguration(SERVICE_ENDPOINT, REGION)
+                )
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
                 .build();
     }
 }
--- src/main/java/com/example/supportapp/repository/PersonRepository.java
+++ src/main/java/com/example/supportapp/repository/PersonRepository.java
@@ -1,8 +1,10 @@
 package com.example.supportapp.repository;
 
 import com.example.supportapp.model.Person;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
+@EnableScan
 public interface PersonRepository extends CrudRepository<Person, String> {
     // Additional custom queries (if needed) can be defined here
 }
